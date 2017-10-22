3cfb9a0d36025e2bab089cf7cf25fc95cd6388ac
Bangsplat <theron@dankerton.net>
2017-10-21 19:42:47 -0700

    simplified working version

--- hexXOR.pl
+++ hexXOR.pl
@@ -2,8 +2,6 @@
 
 use strict;
 use warnings;
-use List::Util qw( min );
-
 
 # hexXOR.pl
 # 
@@ -12,13 +10,24 @@ use List::Util qw( min );
 # 
 # Created 2017-10-21
 # Modified 2017-10-21
-# Version 0.9
+# Version 1.0
 # 
 # written by Theron Trowbridge
 
+sub min($$);
+
 if ( $#ARGV != 1 ) { die "Usage: $0 <string1> <string2>\n"; }
 
-my ( $hex1, $hex2 ) = @ARGV;
+my ( $str1, $str2 ) = @ARGV;
+my $formatString = "H" . min( length( $str1 ), length( $str2 ) );
+my $output = pack( $formatString, $str1 ) ^ pack( $formatString, $str2 );
+my $outputStr = unpack( $formatString, $output );
 
-print "TEST: argument 1: $hex1\n";
-print "TEST: argument 2: $hex2\n";
+print( "$outputStr\n" );
+
+sub min($$) {
+    my $a = shift(@_);
+    my $b = shift(@_);
+    
+    if ( $a < $b ) { return( $a ) } else { return( $b ); }
+}
